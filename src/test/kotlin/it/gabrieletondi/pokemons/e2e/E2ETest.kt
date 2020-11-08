b2c4b0260396dd4cf8cf3b458f64f467fc996fc6
Gabriele Tondi <gabriele.tondi@casavo.com>
2020-11-08 10:17:10 +0100

    Added acceptance test for pokemon API

--- src/test/kotlin/it/gabrieletondi/pokemons/e2e/E2ETest.kt
+++ src/test/kotlin/it/gabrieletondi/pokemons/e2e/E2ETest.kt
@@ -1,8 +1,11 @@
 package it.gabrieletondi.pokemons.e2e
 
 import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.jackson.responseObject
+import org.junit.Ignore
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
@@ -19,8 +22,29 @@ class E2ETest {
         val (_, response, result) = request.responseString()
 
         result.fold(
-            success = { assertEquals(200, response.statusCode) },
-            failure = { fail(it.exception) }
+                success = { assertEquals(200, response.statusCode) },
+                failure = { fail(it.exception) }
         )
     }
+
+    @Test
+    @Disabled("This will work only after implementation")
+    internal fun `known pokemon`() {
+        val request = fuel.get("$baseUrl/pokemon/charizard");
+
+        val (_, response, result) = request.responseObject<PokemonResponse>()
+
+        result.fold(
+                success = {
+                    assertEquals(200, response.statusCode)
+                    assertEquals(PokemonResponse(
+                            "charizard",
+                            "Charizard flies 'round the sky in search of powerful opponents. 't breathes fire of such most wondrous heat yond 't melts aught. However, 't nev'r turns its fiery breath on any opponent weaker than itself."),
+                            result.component1())
+                },
+                failure = { fail(it.exception) }
+        )
+    }
+
+    private data class PokemonResponse(val name: String, val description: String)
 }
